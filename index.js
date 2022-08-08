d1ef0d5592203cad16b317bfe0e6682acc7ff62b
farhan sharif <farhansharif1410@gmail.com>
2022-08-08 21:14:17 -0230

    first and second plm solve

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ let number = 7;
 
 while (number <= 19) {
     if (number % 2 === 1) {
-        console.log(number);
+        // console.log(number);
 
     }
     number++
 }
+
+
+let array = [34, 62, 74, 27, 28, 83, 83, 72, 84]
+const findLength = array.length;
+array[4] = 120;
+console.log(array);
+// console.log(findLength);
