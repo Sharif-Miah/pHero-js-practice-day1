7dbc82d9cb097090827f890ca98ea53d0b58272a
farhan sharif <farhansharif1410@gmail.com>
2022-08-09 11:36:33 -0230

    function 5 No plm solve

--- function.js
+++ function.js
@@ -93,3 +93,23 @@ console.log('He is my close Friend: ', closeName);
 
 
 // ৫. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে। 
+
+
+const array = [20, 48, 29, 74, 26, -44, 27, 71, 56, 26, -92, 273, 290, -263, 92];
+
+function positiveNumber(number) {
+    let findpositiveNumber = [];
+    for (let i = 0; i < number.length; i++) {
+        const element = number[i];
+        // console.log(element);
+        if (element > 0) {
+            findpositiveNumber.push(element);
+        } else {
+            break;
+        }
+    }
+    return findpositiveNumber;
+}
+
+const findPositiveNumberResult = positiveNumber(array);
+console.log(findPositiveNumberResult);
