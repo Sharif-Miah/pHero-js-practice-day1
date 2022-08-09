3469f77fcae39bb9a1efa99fe6133cb65e3de744
farhan sharif <farhansharif1410@gmail.com>
2022-08-08 21:56:18 -0230

    11 no plm solve

---
+++ function.js
@@ -0,0 +1,8 @@
+// ১১. তিনটা সংখ্যার গুনফল বের করে ফাইনাল রেজাল্ট আউটপুট হিসেবে রিটার্ন করতে হবে। তুমি কি সেই কোড লিখতে পারবে। যদি পারো তাহলে সেই কোড লিখে ফেলো।
+
+function multipication(a, b, c) {
+    const result = a * b * c;
+    return result;
+}
+const result1 = multipication(6, 9, 18)
+console.log(result1);
